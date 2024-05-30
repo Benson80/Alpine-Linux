02a15a5f9c56ada289d34d2f3e37c99de2dfc77c
Benson80 <81787444+Benson80@users.noreply.github.com>
2024-05-30 14:45:45 +0800

    Update setup-interfaces.sh

--- setup-interfaces.sh
+++ setup-interfaces.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# 设置网络接口
-setup-interfaces
-
-# 启用eth0接口
-ifup eth0
-
-# 设置DNS
-echo "nameserver 8.8.8.8" > $PREFIX/etc/resolv.conf
-
 # 下载快速安装系统的答案文件
 wget https://gist.githubusercontent.com/oofnikj/e79aef095cd08756f7f26ed244355d62/raw/answerfile -O answerfile
 
